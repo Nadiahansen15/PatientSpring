4cf038559df7eadee6df6a65628401e1cd0060eb
Nadia Hansen <50193195+Nadiahansen15@users.noreply.github.com>
2019-10-11 14:27:53 +0200

    Add files via upload

---
+++ PatientSpring/src/main/java/com/example/patientspring/PatientSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.patientspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientSpringApplication.class, args);
+    }
+
+}
